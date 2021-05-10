7df9f69cf0ca5092f65ecd7054988e2fbd19feac
Aayush <aayush.shah@sakec.ac.in>
2021-05-10 16:42:52 +0530

    initial commit

---
+++ congfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBxTzzf5ZP5rear-NvTDZy8xc4wtgodbFM",
+    authDomain: "bloodbanksystem-7b6e6.firebaseapp.com",
+    projectId: "bloodbanksystem-7b6e6",
+    storageBucket: "bloodbanksystem-7b6e6.appspot.com",
+    messagingSenderId: "677626614616",
+    appId: "1:677626614616:web:28560b7f68aec1f69c4b2d",
+    measurementId: "G-F655215ZQ8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ // firebase.analytics();
